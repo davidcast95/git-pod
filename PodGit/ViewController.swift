8e679b06fe95f3ce95727504e82ba40453cb8058
David Wibisono <davidwibisono95@gmail.com>
2019-07-17 15:49:51 +0700

    Initial Commit

---
+++ PodGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodGit
+//
+//  Created by David-UC on 7/17/19.
+//  Copyright © 2019 David-DAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
